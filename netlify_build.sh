c6b403705ed00466ae017992c0c3f7a6b9356ba7
rakeshlakshmanan <ralakshm@tcd.ie>
2026-04-07 21:29:40 +0100

    Ai changes

---
+++ netlify_build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# Install Flutter
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+export PATH="$PATH:$HOME/flutter/bin"
+
+# Build
+flutter pub get
+flutter build web --release
